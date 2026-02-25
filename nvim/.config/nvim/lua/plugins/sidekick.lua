bdc5d5d39711217bbaae898f2d82ae9552110de0
Aaron Ocampo <ocampocanteroaaron@gmail.com>
2026-02-25 00:35:05 -0300

    sidekick float mode

---
+++ nvim/.config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    cli = {
+      win = {
+        layout = "float",
+        float = {
+          width = 0.8,
+          height = 0.8,
+          row = 0.5,
+          col = 0.5,
+          border = "rounded",
+        },
+      },
+    },
+  },
+}
